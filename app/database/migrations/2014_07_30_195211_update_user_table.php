351c0513ca74cbfbdac7e50a0cce6694e290c72a
Aaron Ryou <aryou@singlehop.com>
2014-07-30 15:15:14 -0500

    [ECIM] - database change

---
+++ app/database/migrations/2014_07_30_195211_update_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('user', function($table){
+			$table->string('phone', 16)->after('email')->default('');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('user', function($table){
+			$table->dropColumn('phone');
+		});
+	}
+
+}
--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -2,27 +2,43 @@
 class UserTableSeeder extends Seeder {
 	public function run() {
 		User::create(array(
-		'first'     		=> 'Aaron',
-		'last'				=> 'Ryou',
-		'username'			=> 'aryou',
-		'email'				=> 'aryou@singlehop.com',
-		'has_two_factor'	=> '1',
-		'password'			=> Hash::make('singlehop'),
+			'first'     		=> 'Aaron',
+			'last'				=> 'Ryou',
+			'username'			=> 'aryou',
+			'email'				=> 'aryou@singlehop.com',
+			'has_two_factor'	=> '1',
+			'password'			=> Hash::make('singlehop'),
 		));		
 		User::create(array(
-		'first'     		=> 'Marcus',
-		'last'				=> 'Hightower',
-		'username'			=> 'mhightower',
-		'email'				=> 'mhightower@singlehop.com',
-		'password'			=> Hash::make('singlehop'),
+			'first'     		=> 'Marcus',
+			'last'				=> 'Hightower',
+			'username'			=> 'mhightower',
+			'email'				=> 'mhightower@singlehop.com',
+			'password'			=> Hash::make('singlehop'),
 		));
 		User::create(array(
-		'first'     		=> 'Ricardo',
-		'last'				=> 'Talavera',
-		'username'			=> 'rtalavera',
-		'email'				=> 'rtalavera@singlehop.com',
-		'has_two_factor'	=> '1',
-		'password'			=> Hash::make('singlehop'),
+			'first'     		=> 'Ricardo',
+			'last'				=> 'Talavera',
+			'username'			=> 'rtalavera',
+			'email'				=> 'rtalavera@singlehop.com',
+			'has_two_factor'	=> '1',
+			'password'			=> Hash::make('singlehop'),
+		));
+		User::create(array(
+			'first'     		=> 'Shawn',
+			'last'				=> 'Scofield',
+			'username'			=> 'sscofield',
+			'email'				=> 'sscofield@singlehop.com',
+			'has_two_factor'	=> '1',
+			'password'			=> Hash::make('singlehop'),
+		));
+		User::create(array(
+			'first'     		=> 'Roger',
+			'last'				=> 'Wakeman',
+			'username'			=> 'rwakeman',
+			'email'				=> 'rwakeman@singlehop.com',
+			'has_two_factor'	=> '1',
+			'password'			=> Hash::make('singlehop'),
 		));		
 	}
 }
